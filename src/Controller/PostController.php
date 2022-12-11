e5b23aba22c5451d30470a0f776ae29eb69f0d71
BidibulleKing <remy.delepaule@gmail.com>
2022-12-11 19:48:02 +0100

    Exercice terminé + bonus => reste à faire la partie formulaire ; ajout d'un custom theme handler côté serveur

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,16 +2,32 @@
 
 namespace App\Controller;
 
+use DateTime;
 use App\Entity\Post;
-use App\Repository\PostRepository;
 use DateTimeImmutable;
+use App\Repository\PostRepository;
+use Doctrine\DBAL\Types\DateType;
+use Doctrine\DBAL\Types\TextType;
 use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\Event\ManagerEventArgs;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class PostController extends AbstractController
 {
+    public function __construct()
+    {
+        $session = new Session();
+        $session->start();
+
+        $this->session = $session;
+        $this->theme = $session->get("theme");
+    }
+
     /**
      * @Route("/posts", name="post_index")
      *
@@ -22,7 +38,8 @@ class PostController extends AbstractController
         $posts = $postRepository->findAll();
 
         return $this->render("main/index.html.twig", [
-            "posts" => $posts
+            "posts" => $posts,
+            "theme" => $this->theme
         ]);
     }
 
@@ -63,8 +80,113 @@ class PostController extends AbstractController
 
         $post = $repository->find($id);
 
-        return $this->render("main/post.html.twig", [
-            "post" => $post
-        ]);
+        if ($this->isPost($post, $id)) {
+            return $this->render("main/post.html.twig", [
+                "post" => $post,
+                "theme" => $this->theme
+            ]);
+        }
+    }
+
+    /**
+     * @Route("/post/{id}/like", name="post_like")
+     *
+     * @param [integer] $id
+     * @return RedirectResponse
+     */
+    public function like($id, ManagerRegistry $doctrine): RedirectResponse
+    {
+        $repository = $doctrine->getRepository(Post::class);
+        $manager = $doctrine->getManager();
+        $datetime = new DateTimeImmutable();
+
+        $post = $repository->find($id);
+
+        if ($this->isPost($post, $id)) {
+            $post->setNbLikes($post->getNbLikes() + 1);
+            $post->setUpdatedAt($datetime);
+
+            $manager->persist($post);
+            $manager->flush();
+
+            return $this->redirect($this->generateUrl("post_show", ["id" => $id]));
+        }
+    }
+
+    /**
+     * @Route("post/new", name="post_create")
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function createAction(Request $request): RedirectResponse
+    {
+        $post = new Post();
+
+        $form = $this->createFormBuilder($post)
+            ->add("title", TextType::class)
+            ->add("body", TextType::class)
+            ->add("created_at", DateType::class)
+            ->add("published_at", DateType::class)
+            ->getForm();
+
+        return $this->redirect($this->generateUrl("post_index"));
+    }
+
+    /**
+     * @Route("/post/{id}/delete", name="post_delete")
+     *
+     * @param [integer] $id
+     * @return RedirectResponse
+     */
+    public function delete($id, ManagerRegistry $doctrine): RedirectResponse
+    {
+        $entityManager = $doctrine->getManager();
+        $repositoryManager = $doctrine->getRepository(Post::class);
+
+        $post = $repositoryManager->find($id);
+
+        if ($this->isPost($post, $id)) {
+            $entityManager->remove($post);
+            $entityManager->flush();
+
+            return $this->redirect($this->generateUrl("post_index"));
+        }
+    }
+
+    /**
+     * @Route("/change-theme", name="change_theme")
+     *
+     * @return RedirectResponse
+     */
+    public function changeTheme(Request $request): RedirectResponse
+    {
+        $session = $this->session;
+
+        if ($session->get("theme") !== null) {
+            if ($session->get("theme") === "dark") {
+                $session->set("theme", "light");
+            } else {
+                $session->set("theme", "dark");
+            }
+        } else {
+            $session->set("theme", "dark");
+        }
+
+        $referer = $request->headers->get("referer");
+
+        return new RedirectResponse($referer);
+    }
+
+    // * UTILS
+    private function isPost($post, $id)
+    {
+        if (!$post) {
+            throw $this->createNotFoundException(
+                "Il n'y a pas de post correspondant à cet identifiant : $id"
+            );
+        }
+
+        return true;
     }
 }
