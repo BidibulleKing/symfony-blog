e96f7afce8b4a57e7d6a4dd2c8eec68e935b03eb
BidibulleKing <remy.delepaule@gmail.com>
2022-12-11 16:11:19 +0100

    Création de vues simples et ajout des fonction index, create et show au controller

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,35 +3,68 @@
 namespace App\Controller;
 
 use App\Entity\Post;
-use DateTime;
+use App\Repository\PostRepository;
 use DateTimeImmutable;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
     /**
-     * @Route("/post", name="post")
+     * @Route("/posts", name="post_index")
+     *
+     * @return Response
      */
-    public function index(ManagerRegistry $doctrine): JsonResponse
+    public function index(PostRepository $postRepository): Response
+    {
+        $posts = $postRepository->findAll();
+
+        return $this->render("main/index.html.twig", [
+            "posts" => $posts
+        ]);
+    }
+
+
+    /**
+     * @Route("/post/create", name="post_create")
+     */
+    public function create(ManagerRegistry $doctrine): Response
     {
         $entityManager = $doctrine->getManager();
 
         $datetime = new DateTimeImmutable();
 
         $post = new Post();
-        $post->setTitle("Hello, Doctrine !")
-            ->setBody("C'est un bel ORM...")
-            ->setNbLikes(100)
+        $post->setTitle("Mon troisième article.")
+            ->setBody("Je vous l'ai dit : je ne m'arrête plus !")
+            ->setNbLikes(2)
             ->setCreatedAt($datetime)
             ->setPublishedAt($datetime);
 
+        $entityManager->persist($post);
+        $entityManager->flush();
 
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/PostController.php',
+
+        return $this->render("main/index.html.twig", [
+            "post" => $post
+        ]);
+    }
+
+    /**
+     * @Route("/post/{id}", name="post_show")
+     *
+     * @return Response
+     */
+    public function show($id, ManagerRegistry $doctrine): Response
+    {
+        $repository = $doctrine->getRepository(Post::class);
+
+        $post = $repository->find($id);
+
+        return $this->render("main/post.html.twig", [
+            "post" => $post
         ]);
     }
 }
